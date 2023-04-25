00064a58c3656633d7a4fb7d895062601cdccd3f
Diego Ortiz <diegortizb@gmail.com>
2023-04-25 08:33:47 -0400

    Función ahora muestra todos los campos vacíos

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -1,16 +1,23 @@
 function validarCampo() {
     let campos = [["name", "nameLbl"], ["correo", "correoLbl"], ["mensaje", "mensajeLbl"]];
-    
+    var vacios = []
+
     for (var i=0; i < campos.length; i++){
 
         let campo = document.forms["contacto"][campos[i][0]].value;
         let etiqueta = document.getElementById(campos[i][1]);
 
+        
         if (campo == "") {
-            alert("Debe llenar el campo " + etiqueta.innerHTML);
-
-            return false;
+            vacios.push(etiqueta.innerHTML)
+            console.log(vacios.length);
         }
     };
+
+    if (vacios.length > 0){
+        alert("Debe llenar los siguientes campos:\n " + vacios.join(", "));
+    }
+    
+    return false;
     
 }
